07b3a000c6065f2a5acb7acbe37ee8750fdbe273
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-25 19:02:55 -0500

    almost there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingComponent,
+    AboutComponent,
+    ServicesComponent,
+    SignupComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
